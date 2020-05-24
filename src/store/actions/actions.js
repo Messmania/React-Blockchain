a74e42243d4ea1eab3356c01202355d814179e29
Monika Sahai <monikasahai03@gmail.com>
2020-05-24 01:45:28 -0400

    Added placeholder for async call on Add action

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -4,8 +4,16 @@ export const SHOW_ALL = "SHOW_ALL";
 export const SORT_BY_RANK = "SORT_BY_RANK";
 export const SORT_BY_PRICE = "SORT_BY_PRICE";
 
-export const add = (id) => {
+const syncAdd = (id) => {
     return { type: ADD_CURRENCY, id }
+}
+export const add = (id) => {
+    return dispatch => {
+        setTimeout(() => { //--replace with API call to get details
+            console.log("Print after 2s");
+            dispatch(syncAdd(id));
+        }, 2000);
+    }
 }
 
 export const remove = (id) => {
